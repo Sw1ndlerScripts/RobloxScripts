0981056f6bc0ea92318537a59fa181009eb1e898
Sw1ndlerScripts <103692349+Sw1ndlerScripts@users.noreply.github.com>
2022-06-17 14:06:46 -0700

    Update Force [Pre-Alpha].lua

--- Force [Pre-Alpha].lua
+++ Force [Pre-Alpha].lua
@@ -172,7 +172,7 @@ local Misc = main:MakeTab({
 })
 
 AutoFarm:AddToggle({
-	Name = "AutoCollect [Run disable fall damage first]",
+	Name = "AutoCollect (BanChance) [Use anti fall damage]",
 	Default = false,
 
 	Callback = function(Value)
